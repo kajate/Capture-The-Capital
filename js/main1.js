553ef10da871abe2829d0078eb0a26db572a7462
Jim Kollevik <jim.kollevik@gmail.com>
2013-10-16 11:27:54 +0200

    updated geolocation and firebase code

--- js/main1.js
+++ js/main1.js
@@ -1,13 +1,14 @@
+var map;
+
 function initialize() {
 
   var mapOptions = {
     zoom: 13,
   };
-  map = new google.maps.Map(document.getElementById('mapCanvas'),
+  map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-  
-
+  // Try HTML5 geolocation
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = new google.maps.LatLng(position.coords.latitude,
@@ -16,7 +17,7 @@ function initialize() {
       var infowindow = new google.maps.InfoWindow({
         map: map,
         position: pos,
-        content: 'This is you'
+        content: 'This is You'
       });
 
       map.setCenter(pos);
@@ -24,6 +25,7 @@ function initialize() {
       handleNoGeolocation(true);
     });
   } else {
+    // Browser doesn't support Geolocation
     handleNoGeolocation(false);
   }
 }
@@ -65,9 +67,8 @@ function handleNoGeolocation(errorFlag) {
   };
 
   var infowindow = new google.maps.InfoWindow(options);
-   map.setOptions({styles: styles});
-   map.setCenter(options.position);
- 
+  map.setCenter(options.position);
+  map.setOptions({styles: styles});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
