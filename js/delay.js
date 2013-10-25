ec7134f1a3981aa88ba4b65acf82ca3ff90591ef
kajate <patrick.gardinger@gmail.com>
2013-10-25 11:31:08 +0200

    CHANGED EVERYTHING

--- js/delay.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var element = document.createElement('script');
-    element.src =
-        'http://maps.google.com/maps/api/js?sensor=true&amp;callback=Initialize';
-    element.type = 'text/javascript';
-    var scripts = document.getElementsByTagName('script')[0];
-    scripts.parentNode.insertBefore(element, scripts);
-}, false);
---
+++ js/navigation.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	$('#nextButton').click(function(){
+
+		$("#one").empty();
+
+		$('#one').append('<div id="one" class="fade"><br><br><br>This is how it works</div>'); 
+
+	// $.getJSON(url, landetImages);
+
+
+	// $.getJSON(url, landetImages);
+
+});
+})
+
+
+// 	function landetImages(data) {
+// 		  
+// 		  for ( var i = 0; i < data.data.length; i++ ) {
+
+// 			   $('#content').append ("<img src="+data.data[i].images.standard_resolution.url+">");
+// 		   }
+// 	 }
+
+
+
+// });
+
+
+
