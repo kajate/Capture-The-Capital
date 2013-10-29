5dcd1390c95b5dca0e7e7230d2818e779101f6a1
kajate <patrick.gardinger@gmail.com>
2013-10-29 17:24:33 +0100

    shits

--- js/navigation.js
+++ js/navigation.js
@@ -6,52 +6,13 @@ $(document).ready(function() {
 
 		$('#one').append('<div id="text-box">Playing as:&nbsp;</div><div id="text-boxfade"> ' + userName  + '</div><div id="text-boxtwo">Online now: </div><div id="text-boxfadetwo">24</div>'); 
 
-		// $('#one').append('<div id="text-box" class="one-third column"><br><br><br>This is how it works<a href="#text-boxgreen" id="togreenButton">TO GREEN</a></div>'); 
-
-	// $.getJSON(url, landetImages);
-
-
-	// $.getJSON(url, landetImages);
-
-
-	// $.getJSON(url, landetImages);
-
-
-	// $.getJSON(url, landetImages);
-
 });
 
 
 
 
-// 	$(document).ready(function() {
-// 	$('#nextnextButton').click(function(){
-
-// 		$("#two").empty();
-
-// 		$('#two').append('<div id="two" class="fade"><br><br><br>This is how it works</div>'); 
-
-// 	// $.getJSON(url, landetImages);
-
-
-// 	// $.getJSON(url, landetImages);
-
-// });
-
 })
 
-
-// 	function landetImages(data) {
-// 		  
-// 		  for ( var i = 0; i < data.data.length; i++ ) {
-
-// 			   $('#content').append ("<img src="+data.data[i].images.standard_resolution.url+">");
-// 		   }
-// 	 }
-
-
-
-// });
 
 
 
